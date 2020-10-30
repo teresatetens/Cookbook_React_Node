0049928c367d0224b6ba839e35408fc5297cf2a8
Abrellr <69308389+Abrellr@users.noreply.github.com>
2020-10-30 09:16:15 +0100

    add Apicture on AboutUs

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,10 +4,13 @@ import "./AboutUs.css"
 function AboutUs() {
     return(
         <div className="container">
-           <h3> Made by </h3>
-           <a>Yodit</a>
-           <a>Teresa</a>
-           <a href="https://www.linkedin.com/in/rosmalina-abrell-98122511a/">Rosmalina</a>
+           <h1 className="madeBy"> Made by </h1>
+           <a className="contact" href="https://www.linkedin.com/in/yodit-gebreheneras-033713168/">Yodit</a>
+           <img className="profil" src="http://localhost:7070/images/avatar.png" alt="yodit"/>
+           <a className="contact" href="https://www.linkedin.com/in/teresatetens/">Teresa</a>
+           <img className="profil" src="http://localhost:7070/images/avatar.png" alt="teresa" />
+           <a className="contact" href="https://www.linkedin.com/in/rosmalina-abrell-98122511a/">Rosmalina</a>
+           <img className="profil" src="http://localhost:7070/images/avatar.png" alt="rosmalina"/>
         </div>
     )
 }
